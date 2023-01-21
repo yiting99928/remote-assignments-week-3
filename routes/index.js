418f91a2abfa5cbe9178f2af63b971c88e7cdd13
yiting <yumy19990628@gmail.com>
2023-01-21 13:34:28 +0800

    revise trackName URL

--- routes/index.js
+++ routes/index.js
@@ -11,28 +11,28 @@ router.get('/myName', (req, res) => {
         res.send(name);
     } else {
         res.send(
-            `<form action="/myName" method="post">
+            `<form action="/trackName" method="post">
             <label>Please enter your name:
-            <input type="text" name="username" />
+            <input type="text" name="name" />
             </label>
             <button type="submit">Submit</button></form>`
         );
     }
 });
 
-router.post('/myName', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect(`/trackName`);
+router.post('/trackName', (req, res) => {
+    res.cookie('username', req.body.name);
+    console.log(req.body.name);
+    res.redirect('/myName')
 });
 
 router.get('/trackName', (req, res) => {
     const name = req.cookies.username;
     const trackName = req.query.name;
-    if (name==trackName) {
-        res.send(trackName)
-    } else{
-        res.redirect('/myName')
-    }
+    name == trackName ?
+        res.send("username:" + trackName)
+        : res.send('no username')
 })
+
 
 module.exports = router;
